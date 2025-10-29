eb484db589e2920df71d016f3c44b98f9df98219
gopinathravichandran <gobinath.ravichandran@twilightitsolutions.com>
2025-10-29 15:24:35 +0530

    fix

--- playwright.config.ts
+++ playwright.config.ts
@@ -41,7 +41,7 @@ export default defineConfig({
       use: {
         browserName: 'chromium',
         channel: 'chrome',
-        headless: false,
+        headless: true,
         screenshot: 'only-on-failure',
         video: 'retain-on-failure',
         trace: 'retain-on-failure',
